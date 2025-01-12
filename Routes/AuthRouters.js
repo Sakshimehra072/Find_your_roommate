ed541f961d84640175f8519100b79d6dd6916f3f
Sakshimehra072 <sakshimehra072@gmail.com>
2025-01-12 23:30:14 +0530

    updated code

--- Routes/AuthRouters.js
+++ Routes/AuthRouters.js
@@ -1,17 +1,12 @@
 const { signup } = require('../controller/AuthController');
 const { signupValidation } = require('../Middlewares/AuthValidation');
 
+const { login } = require('../controller/AuthController');
+const { loginValidation } = require('../Middlewares/AuthValidation');
+
 const router = require('express').Router();
 
-router.post('/login', (req, res) => {
-    res.send('login success');
-});
-
+router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
 module.exports = router;
-
-
-
-// const {signup} = require('../controller/AuthController');
-// const {signupValidation}
--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const UserModel = require('../Models/Users');
 
 const signup = async(req, res) =>{  
@@ -28,6 +29,47 @@ const signup = async(req, res) =>{
 
 }
 
+
+const login = async(req, res) =>{  
+    try{
+        const{ email, password} = req.body;
+        const user = await UserModel.findOne({email});
+        const errorMsg = 'Authentication failed email or password is wrong';
+        if(!user){
+            return res.status(403)
+            .json({message:errorMsg, success: false});
+        }
+
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if(!isPassEqual){
+            return res.status(403)
+            .json({message: errorMsg, success: false});
+        }
+
+        const jwtToken = jwt.sign({email: user.email, _id: user._id},
+            process.env.JWT_SECRET,
+        ) 
+        
+        res.status(201)
+        .json({
+            message : "Login Successfull",
+            success: true,
+            jwtToken,
+            email,
+            name: user.name
+        })
+    }catch(err){
+        res.status(500)
+        .json({
+            message : "Internal Server Error",
+            success: false
+        })
+
+    }
+
+}
+
+
 module.exports =  {
-    signup
+    signup, login
 }
